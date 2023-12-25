42d3b75757709849b4718c6da297693efdd605f2
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-25 22:20:29 +0100

    commit

--- lib/models/text.dart
+++ lib/models/text.dart
@@ -1,1 +1,19 @@
-stless
+import 'package:flutter/material.dart';
+
+class DMSansText extends StatelessWidget {
+  const DMSansText({
+    super.key,
+    this.text,
+    this.fontSize,
+    this.fontWeight,
+    this.color,
+    this.letterSpacing,
+    this.lineHeight,
+    
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text();
+  }
+}
