45fa3ed4243d2a70558b920fe2d665cd2a2978d9
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-25 23:36:21 +0100

    commit

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,5 +1,7 @@
+import 'package:blur/blur.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:yeerlo/data.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -22,12 +24,17 @@ class _SplashScreenState extends State<SplashScreen> {
               Positioned(
                 top: -710,
                 left: -300,
-                child: Container(
-                  width: MediaQuery.of(context).size.width * 2,
-                  height: MediaQuery.of(context).size.width * 2,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
+                child: Blur(
+                  blur: 10,
+                  blurColor: Colors.white.withOpacity(0.1),
+                  
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 2,
+                    height: MediaQuery.of(context).size.width * 2,
+                    decoration: const BoxDecoration(
+                      color: Vx.gray200,
+                      shape: BoxShape.circle,
+                    ),
                   ),
                 ),
               ),
