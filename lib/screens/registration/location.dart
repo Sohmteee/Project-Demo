6acf82a817f58ecd728107d33011b5f06f805918
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 23:27:06 +0100

    commit

--- lib/screens/registration/location.dart
+++ lib/screens/registration/location.dart
@@ -73,7 +73,7 @@ class _LocationScreenState extends State<LocationScreen> {
                 ),
               ),
               SizedBox(height: 30.h),
-              
+              SearchAnchor.bar()
               
               AppTextField(
                 hintText: 'abc@email.com',
