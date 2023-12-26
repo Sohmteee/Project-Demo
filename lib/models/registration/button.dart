8e23871111a61b295ae5601e2e17ca8cc6299d4c
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 15:50:21 +0100

    commit

--- lib/models/registration/button.dart
+++ lib/models/registration/button.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:yeerlo/colors/app_colors.dart';
 import 'package:yeerlo/models/text.dart';
+import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 class ArrowButton extends StatelessWidget {
   const ArrowButton({
@@ -16,44 +17,46 @@ class ArrowButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 270.w,
-      height: 58.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.r),
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            lightOrangeColor,
-            darkOrangeColor,
+    return ZoomTapAnimation(
+      child: Container(
+        width: 270.w,
+        height: 58.h,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.r),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              lightOrangeColor,
+              darkOrangeColor,
+            ],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              blurRadius: 30.r,
+              offset: const Offset(0, 10),
+            ),
           ],
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            blurRadius: 30.r,
-            offset: const Offset(0, 10),
-          ),
-        ],
-      ),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Center(
-            child: AirBnBText(
-              text,
-              color: Colors.white,
-              fontSize: 18.sp,
-              fontWeight: FontWeight.w500,
-              // letterSpacing: 1,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Center(
+              child: AirBnBText(
+                text,
+                color: Colors.white,
+                fontSize: 18.sp,
+                fontWeight: FontWeight.w500,
+                // letterSpacing: 1,
+              ),
             ),
-          ),
-          Positioned(
-            right: 14.w,
-            child: SvgPicture.asset('assets/svg/button-arrow.svg'),
-          ),
-        ],
+            Positioned(
+              right: 14.w,
+              child: SvgPicture.asset('assets/svg/button-arrow.svg'),
+            ),
+          ],
+        ),
       ),
     );
   }
