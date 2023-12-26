5f454a6a6043cf284570ea561213bf1f65b6a1b6
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 21:29:34 +0100

    commit

--- lib/screens/registration/reset_password.dart
+++ lib/screens/registration/reset_password.dart
@@ -50,113 +50,48 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
                 ),
               )
             : null,
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30.w),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: AirBnBText(
-                    'Reset Password',
-                    fontSize: 24.sp,
-                    fontWeight: FontWeight.w500,
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
-                ),
-                SizedBox(height: 21.h),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: DMSansText(
-                    'Please enter your email address to request a password reset code',
-                    color: Theme.of(context).colorScheme.secondary,
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w400,
-                    height: 1.8,
-                  ),
-                ),
-                SizedBox(height: 26.h),
-                AppTextField(
-                  isPassword: true,
-                  hintText: 'Confirm Password',
-                  icon: Icon(
-                    IconlyLight.lock,
-                    size: 22.sp,
-                    color: registrationIconColor,
-                  ),
-                ),
-                SizedBox(height: 26.h),
-                ArrowButton(
-                  text: 'SIGN UP',
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/verification');
-                  },
-                ),
-                SizedBox(height: 24.h),
-                AirBnBText(
-                  'OR',
-                  color: const Color(0xFF9D9898),
-                  fontSize: 18.sp,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: AirBnBText(
+                  'Reset Password',
+                  fontSize: 24.sp,
                   fontWeight: FontWeight.w500,
+                  color: Theme.of(context).colorScheme.secondary,
                 ),
-                SizedBox(height: 5.h),
-                LoginOption(
-                  logo: 'assets/images/logos/google-logo.png',
-                  text: 'Login with Google',
-                  onPressed: () {},
+              ),
+              SizedBox(height: 21.h),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: DMSansText(
+                  'Please enter your email address to request a password reset code',
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w400,
+                  height: 1.8,
                 ),
-                Opacity(
-                  opacity: /* Platform.isIOS ? 1 : 0 */ 1,
-                  child: Column(
-                    children: [
-                      SizedBox(height: 18.h),
-                      LoginOption(
-                        logo: 'assets/images/logos/apple-logo.png',
-                        text: 'Login with Apple ID',
-                        onPressed: () {
-                          if (Platform.isIOS) {}
-                        },
-                      ),
-                    ],
-                  ),
+              ),
+              SizedBox(height: 26.h),
+              AppTextField(
+                hintText: 'abc@email.com',
+                icon: Icon(
+                  IconlyLight.message,
+                  size: 22.sp,
+                  color: registrationIconColor,
                 ),
-                SizedBox(height: 40.h),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text.rich(
-                    TextSpan(
-                      children: [
-                        TextSpan(
-                          text: 'Already have an account?  ',
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.secondary,
-                            fontSize: 16.sp,
-                            fontFamily: 'AirBnB',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        TextSpan(
-                          text: 'Sign in',
-                          style: TextStyle(
-                            color: themeProvider.themeType == ThemeType.light
-                                ? lightOrangeColor
-                                : darkOrangeColor,
-                            fontSize: 16.sp,
-                            fontFamily: 'AirBnB',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                SizedBox(height: 40.h),
-              ],
-            ),
+              ),
+              SizedBox(height: 26.h),
+              ArrowButton(
+                text: 'SEND',
+                onPressed: () {
+                  Navigator.pushNamed(context, '/verification');
+                },
+              ),
+              Expanded(child: child)
+            ],
           ),
         ),
       ),
