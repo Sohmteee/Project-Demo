565f90ed384dd2a4fae88653b041e5d727b1730f
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 14:50:24 +0100

    commit

--- lib/models/registration/switch.dart
+++ lib/models/registration/switch.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppSwitch extends StatefulWidget {
   const AppSwitch({super.key});
@@ -11,7 +12,15 @@ class _AppSwitchState extends State<AppSwitch> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: 32.w,
+      height: 20.h,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.r),
+        border: Border.all(
+          color: Theme.of(context).colorScheme.secondary,
+          width: 1.w,
+        ),
+      ),
     );
   }
 }
