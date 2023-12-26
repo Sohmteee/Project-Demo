b9803f9c64354b5f0f1a2270f658001fa71c03ad
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 02:10:19 +0100

    commit

--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnbaordingScreen extends StatefulWidget {
   const OnbaordingScreen({super.key});
@@ -11,14 +12,30 @@ class _OnbaordingScreenState extends State<OnbaordingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Spacer(),
-            Image.asset('assets/images/onboarding/onb-1.png'),
-            Spacer(flex: 2),
-          ],
-        ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Column(
+            children: [
+              Spacer(),
+              Image.asset('assets/images/onboarding/onb-1.png'),
+              Spacer(flex: 2),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            height: 400.h,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  ligh
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
