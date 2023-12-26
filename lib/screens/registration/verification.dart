5d2ba846fa6d3430381321d05a5a98748d37a155
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 20:52:14 +0100

    commit

--- lib/screens/registration/verification.dart
+++ lib/screens/registration/verification.dart
@@ -74,6 +74,18 @@ class _VerificationScreenState extends State<VerificationScreen> {
                     fontWeight: FontWeight.w400,
                   ),
                 ),
+                SizedBox(height: 27.h),
+                Pinput(
+  defaultPinTheme: defaultPinTheme,
+  focusedPinTheme: focusedPinTheme,
+  submittedPinTheme: submittedPinTheme,
+  validator: (s) {
+    return s == '2222' ? null : 'Pin is incorrect';
+  },
+  pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+  showCursor: true,
+  onCompleted: (pin) => print(pin),
+),
                 SizedBox(height: 40.h),
                 ArrowButton(
                   text: 'CONTINUE',
