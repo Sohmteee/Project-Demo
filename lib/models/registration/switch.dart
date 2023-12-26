b337dc4fe81b9028ba32f73c03779a27f93fa7b0
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 14:53:50 +0100

    commit

--- lib/models/registration/switch.dart
+++ lib/models/registration/switch.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:yeerlo/colors/app_colors.dart';
 
@@ -10,7 +11,8 @@ class AppSwitch extends StatefulWidget {
 }
 
 class _AppSwitchState extends State<AppSwitch> {
-  
+  bool isSwitched = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,6 +24,30 @@ class _AppSwitchState extends State<AppSwitch> {
           color: darkOrangeColor,
           width: 1.w,
         ),
+      ),
+      child: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: 0.2.seconds,
+            curve: Curves.easeIn,
+            left: isSwitched ? 12.w : 0,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isSwitched = !isSwitched;
+                });
+              },
+              child: Container(
+                width: 20.w,
+                height: 20.h,
+                decoration: BoxDecoration(
+                  color: darkOrangeColor,
+                  borderRadius: BorderRadius.circular(95.r),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
