e0fae5ff6e359267e45a4f082cc38613e2a267fe
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 16:04:45 +0100

    commit

--- lib/models/registration/button.dart
+++ lib/models/registration/button.dart
@@ -62,3 +62,61 @@ class ArrowButton extends StatelessWidget {
     );
   }
 }
+
+class LoginOption extends StatelessWidget {
+  const LoginOption({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  final String text;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ZoomTapAnimation(
+      onTap: onPressed,
+      child: Container(
+        width: 270.w,
+        height: 58.h,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.r),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              lightOrangeColor,
+              darkOrangeColor,
+            ],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              blurRadius: 30.r,
+              offset: const Offset(0, 10),
+            ),
+          ],
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Center(
+              child: AirBnBText(
+                text,
+                color: Colors.white,
+                fontSize: 18.sp,
+                fontWeight: FontWeight.w500,
+                // letterSpacing: 1,
+              ),
+            ),
+            Positioned(
+              right: 14.w,
+              child: SvgPicture.asset('assets/svg/button-arrow.svg'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
