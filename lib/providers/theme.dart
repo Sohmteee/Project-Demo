e8fdf23051424745bfcb20148e693067ad825759
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 20:20:54 +0100

    commit

--- lib/providers/theme.dart
+++ lib/providers/theme.dart
@@ -1,1 +1,12 @@
-class ThemeProvider extends C
+import 'package:flutter/material.dart';
+
+enum ThemeType { light, dark }
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode themeMode = ThemeMode.light;
+
+  void toggleTheme(bool isLightTheme) {
+    themeMode = isLightTheme ? ThemeMode.light : ThemeMode.dark;
+    notifyListeners();
+  }
+}
