025a2ad6d2147abe43920d6598b675e01b0830db
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 15:34:35 +0100

    commit

--- lib/models/registration/button.dart
+++ lib/models/registration/button.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ArrowButton extends StatelessWidget {
   const ArrowButton({
     super.key,
     required this.text,
     required this.onPressed,
-
   });
 
   final String text;
@@ -14,7 +14,17 @@ class ArrowButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.r),
+        gradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFFFFA451),
+            Color(0xFFFF7643),
+          ],
+        ),
+      ),
     );
   }
 }
