64f5d95fd5e385ca8c3bb1962cf42d986f47879d
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 15:01:36 +0100

    commit

---
+++ lib/models/registration/switch.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:yeerlo/colors/app_colors.dart';
+
+class AppSwitch extends StatefulWidget {
+  const AppSwitch({super.key});
+
+  @override
+  State<AppSwitch> createState() => _AppSwitchState();
+}
+
+class _AppSwitchState extends State<AppSwitch> {
+  bool isSwitched = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isSwitched = !isSwitched;
+        });
+      },
+      child: AnimatedContainer(
+        duration: 0.2.seconds,
+        width: 32.w,
+        height: 20.h,
+        padding: EdgeInsets.all(2.w),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(95.r),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              lightOrangeColor,
+              darkOrangeColor,
+            ],
+          ),
+          border: Border.all(
+            color: darkOrangeColor,
+            width: 1.w,
+          ),
+        ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            AnimatedPositioned(
+              duration: 0.2.seconds,
+              curve: Curves.easeIn,
+              left: isSwitched ? 12.w : 0,
+              child: Container(
+                width: 14.w,
+                height: 14.h,
+                decoration: BoxDecoration(
+                  color: darkOrangeColor,
+                  borderRadius: BorderRadius.circular(95.r),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
