f336bd4e6c234361e29956201920d574bf5f531d
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 16:22:54 +0100

    commit

--- lib/screens/registration/sign_in.dart
+++ lib/screens/registration/sign_in.dart
@@ -27,137 +27,131 @@ class _SignInScreenState extends State<SignInScreen> {
         decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.primary,
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(),
+        child: ListView(
+          children: [
+            const Spacer(),
+            Image.asset(
+              'assets/images/logos/light-logo.png',
+              width: 174.w,
+              height: 84.h,
+            ).animate(onPlay: (controller) {
+              controller.repeat();
+            }).shimmer(
+              delay: 2.seconds,
+              color: lightOrangeColor,
+              duration: 2.seconds,
+            ),
+            SizedBox(height: 44.h),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: AirBnBText(
+                'Sign in',
+                fontSize: 24.sp,
+                fontWeight: FontWeight.w500,
+                color: themedTextColor,
               ),
-              Image.asset(
-                'assets/images/logos/light-logo.png',
-                width: 174.w,
-                height: 84.h,
-              ).animate(onPlay: (controller) {
-                controller.repeat();
-              }).shimmer(
-                delay: 2.seconds,
-                color: lightOrangeColor,
-                duration: 2.seconds,
-              ),
-              SizedBox(height: 44.h),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: AirBnBText(
-                  'Sign in',
-                  fontSize: 24.sp,
-                  fontWeight: FontWeight.w500,
-                  color: themedTextColor,
+            ),
+            SizedBox(height: 21.h),
+            const AppTextField(
+              hintText: 'abc@email.com',
+              icon: IconlyLight.message,
+            ),
+            SizedBox(height: 19.h),
+            const AppTextField(
+              isPassword: true,
+              hintText: 'Your Password',
+              icon: IconlyLight.lock,
+            ),
+            SizedBox(height: 19.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      rememberMe = !rememberMe;
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      AppSwitch(
+                        value: rememberMe,
+                        onChanged: (value) {
+                          setState(() {
+                            rememberMe = value!;
+                          });
+                        },
+                      ),
+                      SizedBox(width: 6.w),
+                      AirBnBText(
+                        'Remember Me',
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 21.h),
-              const AppTextField(
-                hintText: 'abc@email.com',
-                icon: IconlyLight.message,
-              ),
-              SizedBox(height: 19.h),
-              const AppTextField(
-                isPassword: true,
-                hintText: 'Your Password',
-                icon: IconlyLight.lock,
-              ),
-              SizedBox(height: 19.h),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                AirBnBText(
+                  'Forgot Password?',
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w400,
+                ),
+              ],
+            ),
+            SizedBox(height: 36.h),
+            ArrowButton(
+              text: 'SIGN IN',
+              onPressed: () {
+                // Navigator.pushReplacementNamed(context, '/home');
+              },
+            ),
+            SizedBox(height: 24.h),
+            AirBnBText(
+              'OR',
+              color: const Color(0xFF9D9898),
+              fontSize: 18.sp,
+              fontWeight: FontWeight.w500,
+            ),
+            SizedBox(height: 5.h),
+            LoginOption(
+              logo: 'assets/images/logos/google-logo.png',
+              text: 'Login with Google',
+              onPressed: () {},
+            ),
+            SizedBox(height: 18.h),
+            LoginOption(
+              logo: 'assets/images/logos/apple-logo.png',
+              text: 'Login with Apple ID',
+              onPressed: () {},
+            ),
+            const Spacer(),
+            Text.rich(
+              TextSpan(
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        rememberMe = !rememberMe;
-                      });
-                    },
-                    child: Row(
-                      children: [
-                        AppSwitch(
-                          value: rememberMe,
-                          onChanged: (value) {
-                            setState(() {
-                              rememberMe = value!;
-                            });
-                          },
-                        ),
-                        SizedBox(width: 6.w),
-                        AirBnBText(
-                          'Remember Me',
-                          fontSize: 18.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ],
+                  TextSpan(
+                    text: 'Already have an account?  ',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                      fontSize: 16.sp,
+                      fontFamily: 'AirBnB',
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
-                  AirBnBText(
-                    'Forgot Password?',
-                    fontSize: 18.sp,
-                    fontWeight: FontWeight.w400,
+                  TextSpan(
+                    text: 'Sign in',
+                    style: TextStyle(
+                      color: lightOrangeColor,
+                      fontSize: 16.sp,
+                      fontFamily: 'AirBnB',
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ],
               ),
-              SizedBox(height: 36.h),
-              ArrowButton(
-                text: 'SIGN IN',
-                onPressed: () {
-                  // Navigator.pushReplacementNamed(context, '/home');
-                },
-              ),
-              SizedBox(height: 24.h),
-              AirBnBText(
-                'OR',
-                color: const Color(0xFF9D9898),
-                fontSize: 18.sp,
-                fontWeight: FontWeight.w500,
-              ),
-              SizedBox(height: 5.h),
-              LoginOption(
-                logo: 'assets/images/logos/google-logo.png',
-                text: 'Login with Google',
-                onPressed: () {},
-              ),
-              SizedBox(height: 18.h),
-              LoginOption(
-                logo: 'assets/images/logos/apple-logo.png',
-                text: 'Login with Apple ID',
-                onPressed: () {},
-              ),
-              Expanded(
-                child: Container(),
-              ),
-              Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Already have an account?  ',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondary,
-                        fontSize: 16.sp,
-                        fontFamily: 'AirBnB',
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'Sign in',
-                      style: TextStyle(
-                        color: lightOrangeColor,
-                        fontSize: 16.sp,
-                        fontFamily: 'AirBnB',
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 40.h),
-            ],
-          ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 40.h),
+          ],
         ),
       ),
     );
