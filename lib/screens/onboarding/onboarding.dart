a8df54a9c84817d713c4d02215a92af32e620311
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-26 11:01:02 +0100

    commit

--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -54,136 +54,138 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       );
     });
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          PageView.builder(
-            controller: pageController,
-            scrollDirection: Axis.horizontal,
-            itemCount: 3,
-            itemBuilder: (context, index) {
-              return pages[index];
-            },
-          ),
-          Container(
-            padding: EdgeInsets.all(40.sp),
-            width: double.infinity,
-            height: 250.h,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(40.r),
-              ),
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  lightOrangeColor,
-                  darkOrangeColor,
-                ],
-              ),
+      body: Container(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            PageView.builder(
+              controller: pageController,
+              scrollDirection: Axis.horizontal,
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return pages[index];
+              },
             ),
-            child: Column(
-              children: [
-                DMSansText(
-                  onBoardingData[onBoardingIndex]['title'],
-                  color: Colors.white,
-                  fontSize: 22.sp,
-                  fontWeight: FontWeight.w700,
-                  textAlign: TextAlign.center,
+            Container(
+              padding: EdgeInsets.all(40.sp),
+              width: double.infinity,
+              height: 250.h,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(40.r),
                 ),
-                SizedBox(height: 10.h),
-                DMSansText(
-                  onBoardingData[onBoardingIndex]['description'],
-                  color: Colors.white,
-                  fontSize: 15.sp,
-                  textAlign: TextAlign.center,
-                  height: 1.8,
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    lightOrangeColor,
+                    darkOrangeColor,
+                  ],
                 ),
-                const Spacer(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        if (onBoardingIndex > 0) {
-                          setState(() {
-                            onBoardingIndex--;
-                          });
-                          pageController.previousPage(
-                            duration: 300.milliseconds,
-                            curve: Curves.easeIn,
-                          );
-                        }
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(10.sp),
-                        color: Colors.transparent,
-                        child: AnimatedOpacity(
-                          opacity: (onBoardingIndex == 0) ? 0.5 : 1,
-                          duration: 0.25.seconds,
+              ),
+              child: Column(
+                children: [
+                  DMSansText(
+                    onBoardingData[onBoardingIndex]['title'],
+                    color: Colors.white,
+                    fontSize: 22.sp,
+                    fontWeight: FontWeight.w700,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10.h),
+                  DMSansText(
+                    onBoardingData[onBoardingIndex]['description'],
+                    color: Colors.white,
+                    fontSize: 15.sp,
+                    textAlign: TextAlign.center,
+                    height: 1.8,
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if (onBoardingIndex > 0) {
+                            setState(() {
+                              onBoardingIndex--;
+                            });
+                            pageController.previousPage(
+                              duration: 300.milliseconds,
+                              curve: Curves.easeIn,
+                            );
+                          }
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(10.sp),
+                          color: Colors.transparent,
+                          child: AnimatedOpacity(
+                            opacity: (onBoardingIndex == 0) ? 0.5 : 1,
+                            duration: 0.25.seconds,
+                            child: AirBnBText(
+                              'Skip',
+                              color: Colors.white,
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.w500,
+                              height: 0.10.h,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: List.generate(
+                          3,
+                          (index) {
+                            return AnimatedContainer(
+                              duration: 500.milliseconds,
+                              margin: EdgeInsets.only(right: 10.w),
+                              width: 8.sp,
+                              height: 8.sp,
+                              decoration: BoxDecoration(
+                                color: (index == onBoardingIndex)
+                                    ? Colors.white
+                                    : Colors.white.withOpacity(0.50),
+                                borderRadius: BorderRadius.circular(10.r),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          if (onBoardingIndex < 2) {
+                            setState(() {
+                              onBoardingIndex++;
+                            });
+                            pageController.nextPage(
+                              duration: 300.milliseconds,
+                              curve: Curves.easeIn,
+                            );
+                          } else {
+                            Navigator.pushReplacementNamed(context, '/home');
+                          }
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(10.sp),
+                          color: Colors.transparent,
                           child: AirBnBText(
-                            'Skip',
+                            'Next',
+                            textAlign: TextAlign.right,
                             color: Colors.white,
                             fontSize: 18.sp,
                             fontWeight: FontWeight.w500,
                             height: 0.10.h,
                           ),
                         ),
-                      ),
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: List.generate(
-                        3,
-                        (index) {
-                          return AnimatedContainer(
-                            duration: 500.milliseconds,
-                            margin: EdgeInsets.only(right: 10.w),
-                            width: 8.sp,
-                            height: 8.sp,
-                            decoration: BoxDecoration(
-                              color: (index == onBoardingIndex)
-                                  ? Colors.white
-                                  : Colors.white.withOpacity(0.50),
-                              borderRadius: BorderRadius.circular(10.r),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        if (onBoardingIndex < 2) {
-                          setState(() {
-                            onBoardingIndex++;
-                          });
-                          pageController.nextPage(
-                            duration: 300.milliseconds,
-                            curve: Curves.easeIn,
-                          );
-                        } else {
-                          Navigator.pushReplacementNamed(context, '/home');
-                        }
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(10.sp),
-                        color: Colors.transparent,
-                        child: AirBnBText(
-                          'Next',
-                          textAlign: TextAlign.right,
-                          color: Colors.white,
-                          fontSize: 18.sp,
-                          fontWeight: FontWeight.w500,
-                          height: 0.10.h,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
