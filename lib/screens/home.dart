a2fa5f7175b2fa622b8e5d5fb5eb691be6b45a4c
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-28 00:42:23 +0100

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -147,7 +147,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
         'icon': ThemeSwitch(
           value: themeProvider.themeType == ThemeType.dark,
           onChanged: (value) {
-            // themeProvider.toggleTheme();
+            themeProvider.toggleTheme();
           },
         ),
         'title': 'Theme Mode',
@@ -229,7 +229,6 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
                     onTap: drawerOptions[index]['onTap'],
-                    
                     leading: drawerOptions[index]['icon'],
                     title: Text(
                       drawerOptions[index]['title'],
