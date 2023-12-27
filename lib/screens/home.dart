c06d54a24eb5233651cb1f17ca214b6c9570e4c4
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-27 13:09:48 +0100

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -521,20 +521,20 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                                     ),
                                   ),
                                   Text(
-                                    switch(eventDate.month) {
+                                    switch (eventDate.month) {
                                       1 => 'JAN',
                                       2 => 'FEB',
                                       3 => 'MAR',
                                       4 => 'APR',
                                       5 => 'MAY',
-                                      6 => 'JUN',
-                                      7 => 'JUN',
-                                      8 => 'JUN',
-                                      9 => 'JUN',
-                                      10 => 'JUN',
-                                      6 => 'JUN',
-                                      6 => 'JUN',
-                                      _=>''
+                                      6 => 'JUNE',
+                                      7 => 'JUL',
+                                      8 => 'AUG',
+                                      9 => 'SEP',
+                                      10 => 'OCT',
+                                      11 => 'NOV',
+                                      12 => 'DEC',
+                                      _ => ''
                                     },
                                     style: TextStyle(
                                       color: upcomingEvents[index]
