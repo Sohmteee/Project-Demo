5d0db2a2f11ae24ce146836b03a0e1e66f20248d
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-27 21:28:16 +0100

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -239,6 +239,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
       bottomNavBarColor: Theme.of(context).colorScheme.primary,
       bottomNavItemIconHeight: 20.h,
       bottomNavItemLabelHeight: 15.h,
+      
       fabWidth: 50.w,
       fabIcon: Container(
         height: 54.h,
@@ -269,6 +270,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
           label: 'Explore',
           bottomItemSelectedColor: darkOrangeColor,
           screen: Scaffold(
+            backgroundColor: Theme.of(context).colorScheme.primary,
             appBar: buildAppBar(),
             body: Container(
               color: Theme.of(context).colorScheme.primary,
