57f529bf3230bfbe0e7bef3587720acefde2fbcd
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-27 11:22:26 +0100

    commit

--- lib/screens/registration/location.dart
+++ lib/screens/registration/location.dart
@@ -273,7 +273,7 @@ class _LocationScreenState extends State<LocationScreen> {
                     Row(
                       children: [
                         CountriesFlag(
-                          Flags.unitedStatesOfAmerica,
+                          selectedCountry['name'].l,
                           width: 64.w,
                           height: 64.h,
                         ),
