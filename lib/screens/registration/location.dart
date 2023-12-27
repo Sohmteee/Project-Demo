36e4095ee7d12f0fb958c97b71e246c3c43872d7
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-27 11:24:33 +0100

    commit

--- lib/screens/registration/location.dart
+++ lib/screens/registration/location.dart
@@ -273,7 +273,7 @@ class _LocationScreenState extends State<LocationScreen> {
                     Row(
                       children: [
                         CountriesFlag(
-                          selectedCountry['name'].split('.').toString(),
+                          'assets/flags/${selectedCountry['name'].toLowerCase().replaceAll(' ', '_')}',
                           width: 64.w,
                           height: 64.h,
                         ),
