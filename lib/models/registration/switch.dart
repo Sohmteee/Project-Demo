6f2c36bea67dd03f5490a4c84ba86194ffd34950
sohmteee <ukaegbesomtochukwu@gmail.com>
2023-12-27 22:05:39 +0100

    commit

--- lib/models/registration/switch.dart
+++ lib/models/registration/switch.dart
@@ -67,3 +67,67 @@ class _AppSwitchState extends State<AppSwitch> {
     );
   }
 }
+
+class ThemeSwitch extends StatefulWidget {
+  ThemeSwitch({
+    super.key,
+    required this.value,
+    required this.onChanged,
+  });
+
+  bool value;
+  final Function(bool?) onChanged;
+
+  @override
+  State<ThemeSwitch> createState() => _ThemeSwitchState();
+}
+
+class _ThemeSwitchState extends State<ThemeSwitch> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          widget.value = !widget.value;
+          widget.onChanged(widget.value);
+        });
+      },
+      child: AnimatedContainer(
+        duration: 0.2.seconds,
+        width: 32.w,
+        height: 20.h,
+        padding: EdgeInsets.all(2.w),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(95.r),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              lightOrangeColor,
+              darkOrangeColor,
+            ],
+          ),
+        ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            AnimatedPositioned(
+              duration: 0.2.seconds,
+              curve: Curves.easeIn,
+              left: widget.value ? 12.w : 0,
+              child: Container(
+                width: 16.w,
+                height: 16.h,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(95.r),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
