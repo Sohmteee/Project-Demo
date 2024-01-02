6f9a133e139b1f89ea5f5245de7cffe3b66afa86
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-02 22:34:29 +0100

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -342,7 +342,6 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                   vertical: 8.h,
                 ),
                 child: ListView.builder(
-                  shrinkWrap: ,
                   itemCount: newPopUpItems.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
