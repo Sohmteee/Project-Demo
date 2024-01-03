e4227e0965400596d01e52ddf073695686560a5a
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-03 01:21:15 +0100

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -111,6 +111,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
               SlidingUpPanelWidget(
                 panelController: panelController,
                 controlHeight: 0,
+                
                 child: Container(
                   decoration: BoxDecoration(
                     color: (themeProvider.themeType == ThemeType.dark)
