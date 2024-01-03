e565196ce9200541cc11bb4363f95ece600884bd
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-03 04:56:00 +0100

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -197,7 +197,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                             buildTimeCategories(context),
                             SizedBox(height: 14.h),
                             Padding(
-                              padding: const EdgeInsets.all(8.0),
+                              padding: EdgeInsets.symmetric(horizontal: 20.w),
                               child: Column(
                                 children: [
                                   buildChooseLocation(),
@@ -217,65 +217,63 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
 
   Container buildChooseLocation() {
     return Container(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 18.w,
-                                    vertical: 10.h,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                      width: 1,
-                                      color: const Color(0xFFE6E6E6),
-                                    ),
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: Center(
-                                    child: Row(
-                                      children: [
-                                        ShaderMask(
-                                          blendMode: BlendMode.srcIn,
-                                          shaderCallback: (Rect bounds) =>
-                                              LinearGradient(
-                                            begin: Alignment.topCenter,
-                                            end: Alignment.bottomCenter,
-                                            colors: [
-                                              lightOrangeColor,
-                                              darkOrangeColor,
-                                            ],
-                                            tileMode: TileMode.mirror,
-                                          ).createShader(bounds),
-                                          child: const Icon(
-                                            IconlyBold.calendar,
-                                          ),
-                                        ),
-                                        const Spacer(),
-                                        AirBnBText(
-                                          'Choose from calender',
-                                          textAlign: TextAlign.center,
-                                          color: const Color(0xFF7F7979),
-                                          fontSize: 15.sp,
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                        const Spacer(flex: 4),
-                                        ShaderMask(
-                                          blendMode: BlendMode.srcIn,
-                                          shaderCallback: (Rect bounds) =>
-                                              LinearGradient(
-                                            begin: Alignment.topCenter,
-                                            end: Alignment.bottomCenter,
-                                            colors: [
-                                              lightOrangeColor,
-                                              darkOrangeColor,
-                                            ],
-                                            tileMode: TileMode.mirror,
-                                          ).createShader(bounds),
-                                          child: const Icon(
-                                            Icons.chevron_right,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                );
+      padding: EdgeInsets.symmetric(
+        horizontal: 18.w,
+        vertical: 10.h,
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: const Color(0xFFE6E6E6),
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Row(
+          children: [
+            ShaderMask(
+              blendMode: BlendMode.srcIn,
+              shaderCallback: (Rect bounds) => LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  lightOrangeColor,
+                  darkOrangeColor,
+                ],
+                tileMode: TileMode.mirror,
+              ).createShader(bounds),
+              child: const Icon(
+                IconlyBold.calendar,
+              ),
+            ),
+            const Spacer(),
+            AirBnBText(
+              'Choose from calender',
+              textAlign: TextAlign.center,
+              color: const Color(0xFF7F7979),
+              fontSize: 15.sp,
+              fontWeight: FontWeight.w400,
+            ),
+            const Spacer(flex: 4),
+            ShaderMask(
+              blendMode: BlendMode.srcIn,
+              shaderCallback: (Rect bounds) => LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  lightOrangeColor,
+                  darkOrangeColor,
+                ],
+                tileMode: TileMode.mirror,
+              ).createShader(bounds),
+              child: const Icon(
+                Icons.chevron_right,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   SizedBox buildFilterCategories(BuildContext context) {
