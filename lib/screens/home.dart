b6d55e3a5793cc621bcca260059461debeec0110
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-03 02:28:30 +0100

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -173,11 +173,14 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                       SizedBox(height: 30.h),
                       Padding(
                         padding: EdgeInsets.symmetric(horizontal: 20.w),
-                        child: AirBnBText(
-                          'Time & Date',
-                          color: HexColor('#120D26'),
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w400,
+                        child: Align(
+                          alignment: Alignment.topLeft,
+                          child: AirBnBText(
+                            'Time & Date',
+                            color: HexColor('#120D26'),
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
                       ),
                     ],
