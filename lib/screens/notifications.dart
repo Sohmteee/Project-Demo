42ec7facdffdbb8e6345217490970114ae906180
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-11 17:34:31 +0100

    commit

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:yeerlo/colors/hex_color.dart';
+import 'package:yeerlo/models/text.dart';
 
 class NotificationsScreen extends StatefulWidget {
   const NotificationsScreen({super.key});
@@ -11,7 +14,17 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        elevation: 0,
+        leading: BackButton(color: Theme.of(context).colorScheme.secondary),
+        title: AirBnBText(
+          'Notifications',
+          color: HexColor('#120D26'),
+          fontSize: 24.sp,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+      backgroundColor: Colors.white,
     );
   }
 }
