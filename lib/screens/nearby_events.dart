2edf6b40a25bab280ddf8ed68cba95690a4c531e
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-11 21:30:09 +0100

    commit

--- lib/screens/nearby_events.dart
+++ lib/screens/nearby_events.dart
@@ -7,7 +7,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:yeerlo/colors/app_colors.dart';
-import 'package:yeerlo/colors/hex_color.dart';
 import 'package:yeerlo/models/text.dart';
 import 'package:yeerlo/providers/theme.dart';
 
