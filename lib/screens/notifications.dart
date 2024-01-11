f17fbe1c7a2b37fbc6e799a67b8373b28273980b
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-11 18:32:55 +0100

    commit

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -82,7 +82,9 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                 itemBuilder: (context, index) {
                   DateTime time = provider.notificatons[index]['time'];
                   String timeToString =
-                      DateTime.now()time.minute < 1 ? "Just now" : "${time.minute} min ago";
+                      DateTime.now().difference(time).inMinutes < 1
+                          ? "Just now"
+                          : "${time.minute} min ago";
 
                   return Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
