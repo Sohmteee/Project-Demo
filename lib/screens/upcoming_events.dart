ccbe9d7b96a27c0abfe44c71951601bfdf465982
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-11 20:25:36 +0100

    commit

--- lib/screens/upcoming_events.dart
+++ lib/screens/upcoming_events.dart
@@ -1,13 +1,16 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:yeerlo/colors/app_colors.dart';
 import 'package:yeerlo/colors/hex_color.dart';
 import 'package:yeerlo/models/text.dart';
 import 'package:yeerlo/providers/notifications.dart';
+import 'package:yeerlo/providers/theme.dart';
 
 class UpcomingEventsScreen extends StatefulWidget {
   const UpcomingEventsScreen({super.key});
@@ -20,6 +23,7 @@ class _UpcomingEventsScreenState extends State<UpcomingEventsScreen> {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<NotificationProvider>(context);
+    final themeProvider = Provider.of<ThemeProvider>(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -52,111 +56,107 @@ class _UpcomingEventsScreenState extends State<UpcomingEventsScreen> {
       backgroundColor: Vx.gray100,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.w),
-        child: provider.notificatons.isEmpty
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Spacer(),
-                  SvgPicture.asset(
-                    'assets/svg/no-notification.svg',
-                    width: 136.w,
-                    height: 168.5.h,
-                  ),
-                  SizedBox(height: 40.h),
-                  AirBnBText(
-                    'No UpcomingEvents!',
-                    textAlign: TextAlign.center,
-                    color: Theme.of(context).colorScheme.secondary,
-                    fontSize: 18.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  SizedBox(height: 7.h),
-                  AirBnBText(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor',
-                    textAlign: TextAlign.center,
-                    color: Theme.of(context)
-                        .colorScheme
-                        .secondary
-                        .withOpacity(0.7),
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  const Spacer(flex: 2),
-                ],
-              )
-            : ListView.builder(
-                itemCount: 3,
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) {
-                  DateTime time = provider.notificatons[index]['time'];
-                  String timeToString = DateTime.now()
-                              .difference(time)
-                              .inMinutes <
-                          1
-                      ? "Just now"
-                      : "${DateTime.now().difference(time).inMinutes} min ago";
-
-                  return Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10.h),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SvgPicture.asset(
-                          'assets/svg/notification-profile.svg',
-                          width: 45.w,
-                          height: 45.h,
-                        ),
-                        SizedBox(width: 18.w),
-                        Expanded(
-                          child: Text.rich(
-                            TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: 'David Silbia ',
-                                  style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.secondary,
-                                    fontSize: 14.sp,
-                                    fontFamily: 'DM Sans',
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                TextSpan(
-                                  text: 'commented on your post ',
-                                  style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.secondary,
-                                    fontSize: 14.sp,
-                                    fontFamily: 'DM Sans',
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                                TextSpan(
-                                  text:
-                                      '<<${provider.notificatons[index]['comment']}>>',
-                                  style: TextStyle(
-                                    color: const Color(0xFFFFB459),
-                                    fontSize: 14.sp,
-                                    fontFamily: 'DM Sans',
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        DMSansText(
-                          timeToString,
-                          textAlign: TextAlign.right,
-                          color: HexColor('#3C3E56'),
-                          fontSize: 12.sp,
-                          fontWeight: FontWeight.w400,
-                        )
-                      ],
+        child: Expanded(
+          child: ListView.builder(
+            itemCount: 10,
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                height: 112.h,
+                padding: EdgeInsets.all(10.sp),
+                margin: EdgeInsets.symmetric(vertical: 7.h),
+                decoration: BoxDecoration(
+                  color: themeProvider.themeType == ThemeType.light
+                      ? lightBackgroundColor
+                      : darkBackgroundColor,
+                  borderRadius: BorderRadius.circular(18.r),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color(0x0F4F5588),
+                      blurRadius: 30,
+                      offset: Offset(0, 8),
+                      spreadRadius: 0,
+                    )
+                  ],
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10.r),
+                      child: Image.asset(
+                        'assets/images/home/near-you-2.png',
+                        width: 79.w,
+                        height: 92.h,
+                      ),
                     ),
-                  );
-                },
-              ),
+                    SizedBox(width: 18.w),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              AirBnBText(
+                                '1ST MAY- SAT -2:00 PM',
+                                color: const Color(0xFFFFB459),
+                                fontSize: 12.sp,
+                                fontWeight: FontWeight.w400,
+                              ),
+                              SizedBox(height: 3.h),
+                              AirBnBText(
+                                'International kids safe parents night out',
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontSize: 15.sp,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              SizedBox(height: 5.h),
+                              DMSansText(
+                                'FREE - \$25',
+                                textAlign: TextAlign.center,
+                                color: const Color(0xFFFFB459),
+                                fontSize: 11.sp,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              SvgPicture.asset(
+                                'assets/svg/map-pin.svg',
+                                height: 12.h,
+                                width: 12.w,
+                              ),
+                              SizedBox(width: 5.w),
+                              DMSansText(
+                                'Radius Gallery • Santa Cruz',
+                                color: const Color(0xFF747688),
+                                fontSize: 11,
+                                fontWeight: FontWeight.w400,
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 8.w,
+                      ),
+                      child: Icon(
+                        IconlyLight.bookmark,
+                        size: 16.sp,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
