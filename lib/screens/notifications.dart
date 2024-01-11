8d5966770cdf1384ba9617c671f6f6b7e3494892
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-11 18:18:37 +0100

    commit

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -77,8 +77,104 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                 ],
               )
             : ListView.builder(
+                itemCount: 3,
+                physics: const BouncingScrollPhysics(),
                 itemBuilder: (context, index) {
-                  return 
+                  return Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SvgPicture.asset('assets/svg/notification-profile.svg'),
+                      SizedBox(width: 18.w),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                AirBnBText(
+                                  '1ST MAY- SAT -2:00 PM',
+                                  color: const Color(0xFFFFB459),
+                                  fontSize: 12.sp,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                                SizedBox(height: 3.h),
+                                AirBnBText(
+                                  'Women\'s leadership conference',
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                  fontSize: 15.sp,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                SizedBox(height: 5.h),
+                                DMSansText(
+                                  'FREE - \$25',
+                                  textAlign: TextAlign.center,
+                                  color: const Color(0xFFFFB459),
+                                  fontSize: 11.sp,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/svg/map-pin.svg',
+                                  height: 12.h,
+                                  width: 12.w,
+                                ),
+                                SizedBox(width: 5.w),
+                                DMSansText(
+                                  'Radius Gallery • Santa Cruz',
+                                  color: const Color(0xFF747688),
+                                  fontSize: 11,
+                                  fontWeight: FontWeight.w400,
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          (index == 1)
+                              ? Image.asset(
+                                  'assets/images/home/google-meet-icon.png',
+                                  height: 16.h,
+                                  width: 16.w,
+                                )
+                              : Image.asset(
+                                  'assets/images/home/zoom-icon.png',
+                                  height: 16.h,
+                                  width: 16.w,
+                                  fit: BoxFit.contain,
+                                ),
+                          ZoomTapAnimation(
+                            onTap: () {
+                              setState(() {
+                                isFavourite = !isFavourite;
+                              });
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                left: 8.w,
+                              ),
+                              child: Icon(
+                                isFavourite
+                                    ? IconlyBold.bookmark
+                                    : IconlyLight.bookmark,
+                                size: 16.sp,
+                                color: isFavourite
+                                    ? HexColor('#EB5757')
+                                    : Theme.of(context).colorScheme.secondary,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  );
                 },
               ),
       ),
