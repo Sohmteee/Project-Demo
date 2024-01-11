76520faec63be9eb0a01d84573165ea77e014738
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-11 18:00:06 +0100

    commit

--- lib/providers/notifications.dart
+++ lib/providers/notifications.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 
 class NotificationProvider extends ChangeNotifier{
   List notificatons = [
-
+    {
+      'comment': 'Nice Event',
+      'time': DateTime.now(),
+    },
+    {
+      'comment': 'Nice Event',
+      'time': DateTime.now(),
+    },
+    {
+      'comment': 'Nice Event',
+      'time': DateTime.now(),
+    },
   ];
 }
