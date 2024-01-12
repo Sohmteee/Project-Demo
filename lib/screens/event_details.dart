e6158bf683a1aef92bb2cf6f069caa69065988c5
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-12 17:14:30 +0100

    commit

--- lib/screens/event_details.dart
+++ lib/screens/event_details.dart
@@ -416,55 +416,59 @@ class _EventDetailsScreenState extends State<EventDetailsScreen> {
         ),
         Positioned(
           top: 100.h,
-          child: Row(
-            children: [
-              BackButton(color: Theme.of(context).colorScheme.primary),
-              AirBnBText(
-                'Event Details',
-                color: Theme.of(context).colorScheme.primary,
-                fontSize: 24.sp,
-                fontWeight: FontWeight.w500,
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Container(
-                  height: 36.h,
-                  decoration: ShapeDecoration(
-                    color: Colors.white.withOpacity(0.2),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.r),
+          child: SizedBox(
+            width: double.maxFinite,
+            child: Row(
+              children: [
+                BackButton(color: Theme.of(context).colorScheme.primary),
+                AirBnBText(
+                  'Event Details',
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+                Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: Container(
+                    height: 36.h,
+                    decoration: ShapeDecoration(
+                      color: Colors.white.withOpacity(0.2),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
                     ),
-                  ),
-                  child: Center(
-                    child: SvgPicture.asset(
-                      'assets/svg/send.svg',
-                      height: 18.h,
-                      width: 18.w,
+                    child: Center(
+                      child: SvgPicture.asset(
+                        'assets/svg/send.svg',
+                        height: 18.h,
+                        width: 18.w,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Container(
-                  height: 36.h,
-                  decoration: ShapeDecoration(
-                    color: Colors.white.withOpacity(0.2),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.r),
+                IconButton(
+                  onPressed: () {},
+                  icon: Container(
+                    height: 36.h,
+                    decoration: ShapeDecoration(
+                      color: Colors.white.withOpacity(0.2),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
                     ),
-                  ),
-                  child: Center(
-                    child: SvgPicture.asset(
-                      'assets/svg/bookmark-outline.svg',
-                      height: 15.h,
-                      width: 15.w,
+                    child: Center(
+                      child: SvgPicture.asset(
+                        'assets/svg/bookmark-outline.svg',
+                        height: 15.h,
+                        width: 15.w,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(width: 12.w),
-            ],
+                SizedBox(width: 12.w),
+              ],
+            ),
           ),
         ),
         Positioned(
