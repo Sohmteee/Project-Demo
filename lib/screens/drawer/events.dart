b6c5b4c057842e2a37acf792fd3d805c06976e85
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-12 01:44:40 +0100

    commit

--- lib/screens/drawer/events.dart
+++ lib/screens/drawer/events.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:yeerlo/models/text.dart';
+import 'package:yeerlo/providers/theme.dart';
 
 class EventsScreen extends StatefulWidget {
   const EventsScreen({super.key});
@@ -10,8 +16,31 @@ class EventsScreen extends StatefulWidget {
 class _EventsScreenState extends State<EventsScreen> {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return Scaffold(
-      
+      appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: 60.h,
+        leading: BackButton(color: Theme.of(context).colorScheme.secondary),
+        titleSpacing: 0.w,
+        // backgroundColor: Colors.transparent,
+        title: AirBnBText(
+          'Events',
+          color: Theme.of(context).colorScheme.secondary,
+          fontSize: 24.sp,
+          fontWeight: FontWeight.w500,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
