04a66377abb53a3ecfe483631f09e028bfd0cd42
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-12 02:33:07 +0100

    commit

--- lib/screens/drawer/events.dart
+++ lib/screens/drawer/events.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:yeerlo/colors/app_colors.dart';
