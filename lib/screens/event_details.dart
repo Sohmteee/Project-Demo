c7ab50865a46b1f8c27af23638097774559f127e
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-12 16:28:09 +0100

    commit

--- lib/screens/event_details.dart
+++ lib/screens/event_details.dart
@@ -74,54 +74,58 @@ class _EventDetailsScreenState extends State<EventDetailsScreen> {
         ],
       ),
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            buildImageStack(),
-            SizedBox(height: 20.h),
-            DMSansText(
-              'SAMUEL MEET & GREET PARTY',
-              color: Theme.of(context).colorScheme.secondary,
-              fontSize: 28.sp,
-              fontWeight: FontWeight.w400,
-            ),
-            SizedBox(height: 18.h),
-            buildDetailTilesColumn(context),
-            SizedBox(height: 35.h),
-            buildAboutEvent(context),
-            SizedBox(height: 50.h),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 22.w),
-              child: Row(
-                children: [
-                  Column(
+      body: Column(
+        children: [
+          buildImageStack(),
+          SizedBox(height: 20.h),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                DMSansText(
+                  'SAMUEL MEET & GREET PARTY',
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 28.sp,
+                  fontWeight: FontWeight.w400,
+                ),
+                SizedBox(height: 18.h),
+                buildDetailTilesColumn(context),
+                SizedBox(height: 35.h),
+                buildAboutEvent(context),
+                SizedBox(height: 50.h),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 22.w),
+                  child: Row(
                     children: [
-                      ShaderMask(
-                        blendMode: BlendMode.srcIn,
-                        shaderCallback: (Rect bounds) => LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            lightOrangeColor,
-                            darkOrangeColor,
-                          ],
-                          tileMode: TileMode.mirror,
-                        ).createShader(bounds),
-                        child: AirBnBText(
-                          'REVIEWS',
-                          textAlign: TextAlign.center,
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w400,
-                          height: 0,
-                        ),
+                      Column(
+                        children: [
+                          ShaderMask(
+                            blendMode: BlendMode.srcIn,
+                            shaderCallback: (Rect bounds) => LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                lightOrangeColor,
+                                darkOrangeColor,
+                              ],
+                              tileMode: TileMode.mirror,
+                            ).createShader(bounds),
+                            child: AirBnBText(
+                              'REVIEWS',
+                              textAlign: TextAlign.center,
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w400,
+                              height: 0,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
-            )
-          ],
-        ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
