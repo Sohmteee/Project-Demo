b6c63761a9bbd536eea0af786caeb048da941293
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-12 02:19:24 +0100

    commit

--- lib/screens/drawer/events.dart
+++ lib/screens/drawer/events.dart
@@ -167,8 +167,8 @@ class _EventsScreenState extends State<EventsScreen> {
             ),
           ),
           Spacer(),
-          Image.asset(
-            'assets/images/no-event.png',
+          SvgPicture.asset(
+            'assets/svg/no-event.svg',
             width: 200.w,
             height: 200.h,
           ),
