1d39cf8e559d3a3d439d6aed9c5a009715a67015
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-12 03:47:26 +0100

    commit

--- lib/screens/event_details.dart
+++ lib/screens/event_details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:yeerlo/models/text.dart';
 
 class EventDetailsScreen extends StatefulWidget {
   const EventDetailsScreen({super.key});
@@ -11,6 +13,30 @@ class _EventDetailsScreenState extends State<EventDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: 60.h,
+        leading: BackButton(color: Theme.of(context).colorScheme.primary),
+        titleSpacing: 0.w,
+        backgroundColor: Colors.transparent,
+        title: AirBnBText(
+          'Event Details',
+          color: Theme.of(context).colorScheme.primary,
+          fontSize: 24.sp,
+          fontWeight: FontWeight.w500,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
       body: Column(
         children: [],
       ),
