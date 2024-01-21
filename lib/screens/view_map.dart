1ce92b6b55263b03ab7b1194517e67edcb208d35
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-21 19:12:05 +0100

    commit

---
+++ lib/screens/view_map.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewMap extends StatefulWidget {
+  const ViewMap({super.key});
+
+  @override
+  State<ViewMap> createState() => _ViewMapState();
+}
+
+class _ViewMapState extends State<ViewMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
