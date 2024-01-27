db822a8388afb29ff72139bdf9847b3c26be5660
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-27 06:44:12 +0100

    commit

---
+++ lib/screens/get_ticket.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetTicketScreen extends StatefulWidget {
+  const GetTicketScreen({super.key});
+
+  @override
+  State<GetTicketScreen> createState() => _GetTicketScreenState();
+}
+
+class _GetTicketScreenState extends State<GetTicketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
