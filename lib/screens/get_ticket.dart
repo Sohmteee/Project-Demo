34fcfc7ec0c6fd09a5feebb6d50173482fdea9e0
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-27 06:52:24 +0100

    commit

--- lib/screens/get_ticket.dart
+++ lib/screens/get_ticket.dart
@@ -11,7 +11,20 @@ class _GetTicketScreenState extends State<GetTicketScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        title: Text(
+          'Get a Ticket',
+          style: TextStyle(
+            color: Color(0xFF110C26),
+            fontSize: 24,
+            fontFamily: 'DM Sans',
+            fontWeight: FontWeight.w400,
+            height: 0,
+          ),
+        ),
+      ),
+      body: Column(),
     );
   }
 }
