8074a7f42d9752400f950178ad4b9b8b1746b598
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-01-27 09:20:17 +0100

    commit

--- lib/screens/get_ticket.dart
+++ lib/screens/get_ticket.dart
@@ -53,86 +53,98 @@ class _GetTicketScreenState extends State<GetTicketScreen> {
       itemBuilder: (BuildContext context, int index) {
         return Column(
           children: [
-            Container(
-              height: 300.h,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(19.r),
-                border: Border.all(
-                  color: selectedOption == index
-                      ? lightOrangeColor
-                      : Colors.transparent,
+            ShaderMask(
+              blendMode: BlendMode.srcIn,
+              shaderCallback: (Rect bounds) => LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  darkOrangeColor,
+                  lightOrangeColor,
+                ],
+                tileMode: TileMode.mirror,
+              ).createShader(bounds),
+              child: Container(
+                height: 300.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(19.r),
+                  border: Border.all(
+                    color: selectedOption == index
+                        ? darkOrangeColor
+                        : Colors.transparent,
+                  ),
                 ),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    width: double.maxFinite,
-                    height: 50.h,
-                    decoration: BoxDecoration(
-                      color: selectedOption == index
-                          ? null
-                          : HexColor('#D8D8D8').withOpacity(.46),
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(18.r),
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.maxFinite,
+                      height: 50.h,
+                      decoration: BoxDecoration(
+                        color: selectedOption == index
+                            ? null
+                            : HexColor('#D8D8D8').withOpacity(.46),
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(18.r),
+                        ),
+                        gradient: selectedOption == index
+                            ? LinearGradient(
+                                colors: [
+                                  lightOrangeColor,
+                                  darkOrangeColor,
+                                ],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                              )
+                            : null,
                       ),
-                      gradient: selectedOption == index
-                          ? LinearGradient(
-                              colors: [
-                                lightOrangeColor,
-                                darkOrangeColor,
-                              ],
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                            )
-                          : null,
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20.w),
-                      child: Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            DMSansText(
-                              priceTitles[index],
-                              color: selectedOption == index
-                                  ? Colors.white
-                                  : Theme.of(context).colorScheme.secondary,
-                              fontSize: 16.sp,
-                              fontWeight: FontWeight.w500,
-                            ),
-                            Container(
-                              width: 20.w,
-                              height: 20.h,
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                                shape: BoxShape.circle,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.w),
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              DMSansText(
+                                priceTitles[index],
+                                color: selectedOption == index
+                                    ? Colors.white
+                                    : Theme.of(context).colorScheme.secondary,
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.w500,
                               ),
-                              child: selectedOption == index
-                                  ? ShaderMask(
-                                      blendMode: BlendMode.srcIn,
-                                      shaderCallback: (Rect bounds) =>
-                                          LinearGradient(
-                                        begin: Alignment.topCenter,
-                                        end: Alignment.bottomCenter,
-                                        colors: [
-                                          lightOrangeColor,
-                                          darkOrangeColor,
-                                        ],
-                                        tileMode: TileMode.mirror,
-                                      ).createShader(bounds),
-                                      child: Icon(
-                                        Icons.check,
-                                        size: 16.sp,
-                                      ),
-                                    )
-                                  : null,
-                            ),
-                          ],
+                              Container(
+                                width: 20.w,
+                                height: 20.h,
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: selectedOption == index
+                                    ? ShaderMask(
+                                        blendMode: BlendMode.srcIn,
+                                        shaderCallback: (Rect bounds) =>
+                                            LinearGradient(
+                                          begin: Alignment.topCenter,
+                                          end: Alignment.bottomCenter,
+                                          colors: [
+                                            lightOrangeColor,
+                                            darkOrangeColor,
+                                          ],
+                                          tileMode: TileMode.mirror,
+                                        ).createShader(bounds),
+                                        child: Icon(
+                                          Icons.check,
+                                          size: 16.sp,
+                                        ),
+                                      )
+                                    : null,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
