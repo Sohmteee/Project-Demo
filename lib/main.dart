dc63cb9f38a15faad48a735a2706f114459bfb6d
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-03 19:07:46 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -25,7 +26,7 @@ import 'package:yeerlo/screens/view_map.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Fireba
+  Firebase.initializeApp();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
