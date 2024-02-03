706efc0a6c1f03271beddab784a9a12f91cde198
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-03 17:56:19 +0100

    commit

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -228,7 +228,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
       children: [
         Container(
           width: double.maxFinite,
-          color: greyTextColor,
+          color: linkTextColor.withOpacity(.4),
           padding: EdgeInsets.symmetric(
             vertical: 18.h,
             horizontal: 27.w,
