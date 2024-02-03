7cce84f64701c51f1dca08f69f5f289e92f19679
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-03 07:21:37 +0100

    commit

--- lib/screens/get_ticket.dart
+++ lib/screens/get_ticket.dart
@@ -378,10 +378,7 @@ class _GetTicketScreenState extends State<GetTicketScreen> {
     return ListView.separated(
       itemCount: 3,
       physics: const BouncingScrollPhysics(),
-      padding: EdgeInsets.symmetric(
-        vertical: 22.sp,
-        horizontal: 16.w,
-      ),
+      padding: EdgeInsets.fromLTRB(16.w, 22.h, 16.w, 90.h),
       itemBuilder: (BuildContext context, int index) {
         return Container(
           height: 215.h,
