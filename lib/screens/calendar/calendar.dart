1ae4839dac7f646898f9a6a6347ebfcc0d207693
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-08 05:38:30 +0100

    commit

--- lib/screens/calendar/calendar.dart
+++ lib/screens/calendar/calendar.dart
@@ -227,153 +227,160 @@ class _CalendarScreenState extends State<CalendarScreen> {
                 return SizedBox(height: 20.h);
               },
               itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          width: 43.w,
-                          height: 43.h,
-                          decoration: BoxDecoration(
-                            color: markerColors[index].withOpacity(.16),
-                            borderRadius: BorderRadius.circular(10.r),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              AirBnBText(
-                                switch (_selectedDay.month) {
-                                  1 => 'JAN',
-                                  2 => 'FEB',
-                                  3 => 'MAR',
-                                  4 => 'APR',
-                                  5 => 'MAY',
-                                  6 => 'JUNE',
-                                  7 => 'JUL',
-                                  8 => 'AUG',
-                                  9 => 'SEP',
-                                  10 => 'OCT',
-                                  11 => 'NOV',
-                                  12 => 'DEC',
-                                  _ => ''
-                                },
-                                color: markerColors[index],
-                                fontSize: 13.sp,
-                              ),
-                              AirBnBText(
-                                '${_selectedDay.day.toString().length == 1 ? '0' : ''}${_selectedDay.day}',
-                                color: markerColors[index],
-                                fontSize: 13.sp,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          width: 5.w,
-                        ),
-                        AirBnBText(
-                          '${switch (_selectedDay.weekday) {
-                            1 => 'Mon',
-                            2 => 'Tues',
-                            3 => 'Wed',
-                            4 => 'Thurs',
-                            5 => 'Fri',
-                            6 => 'Sat',
-                            7 => 'Sun',
-                            _ => '',
-                          }}, ${_selectedDay.day}${_selectedDay.day.toString().length == 2 && _selectedDay.day.toString()[0] == '1' ? 'th' : switch (_selectedDay.day.toString()[_selectedDay.day.toString().length - 1]) {
-                              '1' => 'st',
-                              '2' => 'nd',
-                              '3' => 'rd',
-                              _ => 'th',
-                            }} ${switch (_selectedDay.month) {
-                            1 => 'JANUARY',
-                            2 => 'FEBRUARY',
-                            3 => 'MARCH',
-                            4 => 'APRIL',
-                            5 => 'MAY',
-                            6 => 'JUNE',
-                            7 => 'JULY',
-                            8 => 'AUGUST',
-                            9 => 'SEPTEMBER',
-                            10 => 'OCTOBER',
-                            11 => 'NOVEMBER',
-                            12 => 'DECEMBER',
-                            _ => ''
-                          }} ${_selectedDay.year}',
-                          color: markerColors[index],
-                          fontSize: 15.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20.h),
-                    Container(
-                      padding: EdgeInsets.all(20.sp),
-                      decoration: BoxDecoration(
-                        color: HexColor('#FF8D5E'),
-                        borderRadius: BorderRadius.circular(10.r),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    width: 10.sp,
-                                    height: 10.sp,
-                                    margin:
-                                        EdgeInsets.symmetric(horizontal: 1.w),
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      color: Colors.transparent,
-                                      border: Border.all(
-                                        color: markerColors[index],
-                                        width: 2.sp,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(width: 8.h),
-                                  DMSansText(
-                                    '10:00 - 13:00',
-                                    fontSize: 12.sp,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              ),
-                              Icon(
-                                Icons.more_horiz,
-                                color: Colors.white,
-                                size: 13.sp,
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 10.h),
-                          DMSansText(
-                            _getEventsForDay(_selectedDay)[index].title,
-                            color: Colors.white,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          SizedBox(height: 5.h),
-                          DMSansText(
-                            _getEventsForDay(_selectedDay)[index].description,
-                            color: Colors.white,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
+                return buildEventTIle(index);
               },
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column buildEventTIle(int index) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              width: 43.w,
+              height: 43.h,
+              decoration: BoxDecoration(
+                color: markerColors[index].withOpacity(.16),
+                borderRadius: BorderRadius.circular(10.r),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AirBnBText(
+                    switch (_selectedDay.month) {
+                      1 => 'JAN',
+                      2 => 'FEB',
+                      3 => 'MAR',
+                      4 => 'APR',
+                      5 => 'MAY',
+                      6 => 'JUNE',
+                      7 => 'JUL',
+                      8 => 'AUG',
+                      9 => 'SEP',
+                      10 => 'OCT',
+                      11 => 'NOV',
+                      12 => 'DEC',
+                      _ => ''
+                    },
+                    color: markerColors[index],
+                    fontSize: 13.sp,
+                  ),
+                  AirBnBText(
+                    '${_selectedDay.day.toString().length == 1 ? '0' : ''}${_selectedDay.day}',
+                    color: markerColors[index],
+                    fontSize: 13.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 5.w,
+            ),
+            AirBnBText(
+              '${switch (_selectedDay.weekday) {
+                1 => 'Mon',
+                2 => 'Tues',
+                3 => 'Wed',
+                4 => 'Thurs',
+                5 => 'Fri',
+                6 => 'Sat',
+                7 => 'Sun',
+                _ => '',
+              }}, ${_selectedDay.day}${_selectedDay.day.toString().length == 2 && _selectedDay.day.toString()[0] == '1' ? 'th' : switch (_selectedDay.day.toString()[_selectedDay.day.toString().length - 1]) {
+                  '1' => 'st',
+                  '2' => 'nd',
+                  '3' => 'rd',
+                  _ => 'th',
+                }} ${switch (_selectedDay.month) {
+                1 => 'JANUARY',
+                2 => 'FEBRUARY',
+                3 => 'MARCH',
+                4 => 'APRIL',
+                5 => 'MAY',
+                6 => 'JUNE',
+                7 => 'JULY',
+                8 => 'AUGUST',
+                9 => 'SEPTEMBER',
+                10 => 'OCTOBER',
+                11 => 'NOVEMBER',
+                12 => 'DECEMBER',
+                _ => ''
+              }} ${_selectedDay.year}',
+              color: markerColors[index],
+              fontSize: 15.sp,
+              fontWeight: FontWeight.bold,
+            ),
+          ],
+        ),
+        SizedBox(height: 20.h),
+        buildEventBox(index),
+      ],
+    );
+  }
+
+  Container buildEventBox(int index) {
+    return Container(
+      padding: EdgeInsets.all(20.sp),
+      decoration: BoxDecoration(
+        color: HexColor('#FF8D5E'),
+        borderRadius: BorderRadius.circular(10.r),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 10.sp,
+                    height: 10.sp,
+                    margin: EdgeInsets.symmetric(horizontal: 1.w),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.transparent,
+                      border: Border.all(
+                        color: markerColors[index],
+                        width: 2.sp,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8.h),
+                  DMSansText(
+                    '10:00 - 13:00',
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+              Icon(
+                Icons.more_horiz,
+                color: Colors.white,
+                size: 13.sp,
+              ),
+            ],
+          ),
+          SizedBox(height: 10.h),
+          DMSansText(
+            _getEventsForDay(_selectedDay)[index].title,
+            color: Colors.white,
+            fontSize: 16.sp,
+            fontWeight: FontWeight.w600,
+          ),
+          SizedBox(height: 5.h),
+          DMSansText(
+            _getEventsForDay(_selectedDay)[index].description,
+            color: Colors.white,
+            fontSize: 12.sp,
+            fontWeight: FontWeight.w400,
           ),
         ],
       ),
