66357a3bcf9902ad8c48f5bf6a52c6dc7864f5af
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-08 04:58:43 +0100

    commit

--- lib/screens/calendar/calendar.dart
+++ lib/screens/calendar/calendar.dart
@@ -279,10 +279,10 @@ class _CalendarScreenState extends State<CalendarScreen> {
                           6 => 'Sat',
                           7 => 'Sun',
                           _ => '',
-                        }}, _selectedDay.day${switch (_selectedDay.day.toString()[_selectedDay.day.toString().length - 1]) {
-                          '1'=>'st',
-                          '2'=>'nd',
-                          '3'=>'rd',
+                        }}, ${_selectedDay.day}${switch (_selectedDay.day.toString()[_selectedDay.day.toString().length - 1]) {
+                          '1' => 'st',
+                          '2' => 'nd',
+                          '3' => 'rd',
                           _ => 'th',
                         }}'),
                       ],
