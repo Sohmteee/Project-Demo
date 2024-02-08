9d4000ab1a0e20cacfb02c582fa7954eba8f035d
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-08 05:07:32 +0100

    commit

--- lib/screens/calendar/calendar.dart
+++ lib/screens/calendar/calendar.dart
@@ -306,6 +306,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
                         ),
                       ],
                     ),
+                    SizedBox(height: 20.h),
                   ],
                 );
               },
