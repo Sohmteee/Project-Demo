8f8467d54c657b0f1e7539d7a8cdf5c5fa778f38
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-17 13:25:59 +0100

    commit

--- lib/screens/my_tickets/view_ticket.dart
+++ lib/screens/my_tickets/view_ticket.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yeerlo/colors/app_colors.dart';
 
 class ViewTicketScreen extends StatefulWidget {
   const ViewTicketScreen({super.key});
@@ -11,7 +12,11 @@ class _ViewTicketScreenState extends State<ViewTicketScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [lightOrangeColor, darkOrangeColor])
+        ),
+      ),
     );
   }
 }
