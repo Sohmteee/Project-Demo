4a2313bf7f900419f7f2200f94dc704724673a0f
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-29 19:08:15 +0100

    commit

---
+++ lib/screens/drawer/upgrade.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UpgradeScreen extends StatelessWidget {
+  const UpgradeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
