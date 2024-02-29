28a8081df58b8283979effd2d553028aa91f9f37
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-29 16:59:40 +0100

    commit

--- lib/screens/drawer/profile/settings.dart
+++ lib/screens/drawer/profile/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:yeerlo/colors/hex_color.dart';
 import 'package:yeerlo/models/text.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -35,6 +36,22 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
           SizedBox(height: 40.h),
           const Divider(),
+          ListTile(
+            title: DMSansText(
+              'Account',
+              color: Theme.of(context).colorScheme.secondary,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.w400,
+            ),
+            trailing: Icon(
+              Icons.chevron_right,
+              color: Theme.of(context).colorScheme.secondary,
+              size: 24.sp,
+            ),
+          ),
+          Divider(
+            color: HexColor('#D8D8D8'),
+          ),
           ListTile(
             title: DMSansText(
               'Account',
