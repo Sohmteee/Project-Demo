9d28007c225b6bade99257f7b103718f26ba1c30
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-29 15:28:36 +0100

    commit

--- lib/screens/drawer/profile/transfer_success.dart
+++ lib/screens/drawer/profile/transfer_success.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:yeerlo/colors/app_colors.dart';
 
 class TransferSuccessScreen extends StatelessWidget {
   const TransferSuccessScreen({super.key});
@@ -6,6 +8,21 @@ class TransferSuccessScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: lightOrangeColor,
+      appBar: AppBar(
+        elevation: 0,
+        titleSpacing: 0,
+        backgroundColor: Colors.transparent,
+        leading: const BackButton(color: Colors.white),
+        title: Text(
+          'Wallet',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24.sp,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
       body: Container(),
     );
   }
