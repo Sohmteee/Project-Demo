26c8f7b709e3531dd78289ac0f088842f40eea2d
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-29 17:23:17 +0100

    commit

--- lib/screens/drawer/profile/settings/edit_profile.dart
+++ lib/screens/drawer/profile/settings/edit_profile.dart
@@ -1,1 +1,18 @@
-stless
+import 'package:flutter/material.dart';
+
+class EditProfileScreen extends StatelessWidget {
+  const EditProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: BackButton(color: Theme.of(context).colorScheme.secondary),
+      ),
+      body: const Column(
+        children: [],
+      ),
+    );
+  }
+}
