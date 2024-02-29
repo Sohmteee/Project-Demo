39bd315bb4cf29d26364201ce47aae9a73031a57
sohmteee <ukaegbesomtochukwu@gmail.com>
2024-02-29 16:16:55 +0100

    commit

--- lib/screens/drawer/friends.dart
+++ lib/screens/drawer/friends.dart
@@ -78,7 +78,15 @@ class _FindFriendsScreenState extends State<FindFriendsScreen> {
   @override
   void initState() {
     super.initState();
+
     pageController = PageController();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      pageController.addListener(() {
+        setState(() {
+          pageIndex = pageController.page!.round();
+        });
+      });
+    });
   }
 
   @override
@@ -184,73 +192,70 @@ class _FindFriendsScreenState extends State<FindFriendsScreen> {
                         );
                       },
                     ),
-                    Expanded(
-                      // height: MediaQuery.of(context).size.height * .6,
-                      child: ListView.builder(
-                        itemCount: friends.length,
-                        padding: EdgeInsets.only(
-                          top: 16.h,
-                          bottom: MediaQuery.of(context).size.width * .3,
-                          left: 24.w,
-                          right: 24.w,
-                        ),
-                        physics: const BouncingScrollPhysics(),
-                        itemBuilder: (BuildContext context, int index) {
-                          return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                isTicked[index] = !isTicked[index];
-                              });
-                            },
-                            child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 8.h),
-                              color: Colors.transparent,
-                              child: Row(
-                                children: [
-                                  CircleAvatar(
-                                    radius: 22.5.r,
-                                    backgroundColor: Colors.white,
-                                    backgroundImage: Image.asset(
-                                      friends[index]['image'],
-                                      height: 45.h,
-                                      width: 45.w,
-                                      fit: BoxFit.fitWidth,
-                                    ).image,
-                                  ),
-                                  SizedBox(width: 12.w),
-                                  Expanded(
-                                    child: Padding(
-                                      padding:
-                                          EdgeInsets.symmetric(vertical: 3.h),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          AirBnBText(
-                                            friends[index]['name'],
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .secondary,
-                                            fontSize: 14.sp,
-                                            fontWeight: FontWeight.w500,
-                                          ),
-                                          AirBnBText(
-                                            '${friends[index]['followers']} followers',
-                                            textAlign: TextAlign.right,
-                                            color: gray400,
-                                            fontSize: 13.sp,
-                                            fontWeight: FontWeight.w400,
-                                          ),
-                                        ],
-                                      ),
+                    ListView.builder(
+                      itemCount: friends.length,
+                      padding: EdgeInsets.only(
+                        top: 16.h,
+                        bottom: MediaQuery.of(context).size.width * .3,
+                        left: 24.w,
+                        right: 24.w,
+                      ),
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isTicked[index] = !isTicked[index];
+                            });
+                          },
+                          child: Container(
+                            padding: EdgeInsets.symmetric(vertical: 8.h),
+                            color: Colors.transparent,
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 22.5.r,
+                                  backgroundColor: Colors.white,
+                                  backgroundImage: Image.asset(
+                                    friends[index]['image'],
+                                    height: 45.h,
+                                    width: 45.w,
+                                    fit: BoxFit.fitWidth,
+                                  ).image,
+                                ),
+                                SizedBox(width: 12.w),
+                                Expanded(
+                                  child: Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(vertical: 3.h),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        AirBnBText(
+                                          friends[index]['name'],
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .secondary,
+                                          fontSize: 14.sp,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        AirBnBText(
+                                          '${friends[index]['followers']} followers',
+                                          textAlign: TextAlign.right,
+                                          color: gray400,
+                                          fontSize: 13.sp,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
-                          );
-                        },
-                      ),
+                          ),
+                        );
+                      },
                     ),
                   ],
                 ),
